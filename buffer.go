0f55a3decfa678dd16f66a7e56be48a2226bb56b
sfoster <stephen.foster.abq@gmail.com>
2024-06-21 16:23:59 -0600

    finished

--- buffer.go
+++ buffer.go
@@ -1,5 +1,14 @@
 package buffer
 
+type direction int
+
+const (
+	right direction = iota
+	left
+)
+
+const defaultLen = 0
+
 type Buffered[T any] interface {
 	Data() []T
 	Len() int
@@ -20,6 +29,54 @@ func (b *Buffer[T]) Data() []T {
 	return d
 }
 
+func (b *Buffer[T]) AppendSlice(s []T) {
+	l := len(s)
+
+	if b.hasSpace(l) {
+		b.data = append(b.data, s...)
+	} else {
+		panic("slice s exceeds capacity")
+	}
+}
+
+func (b *Buffer[T]) Append(s T) {
+
+	if !b.IsFull() {
+		b.data = append(b.data, s)
+	} else {
+		panic("Append(s T): buffer is full")
+	}
+}
+
+func (b *Buffer[T]) PopRight() T {
+	if !b.IsEmpty() {
+		i := b.Len() - 1
+		v := b.data[i]
+		d := b.data[:i]
+		b.data = d
+		return v
+	} else {
+		panic("buffer is empty")
+	}
+}
+
+func (b *Buffer[T]) PopLeft() T {
+	if !b.IsEmpty() {
+		i := 0
+		v := b.data[i]
+		d := b.data[i+1:]
+		b.data = d
+		return v
+	} else {
+		panic("buffer is empty")
+	}
+}
+
+func (b *Buffer[T]) hasSpace(n int) bool {
+	tl := n + b.Len()
+	return tl <= b.Cap()
+}
+
 func (b *Buffer[T]) Len() int {
 	return len(b.data)
 }
@@ -34,23 +91,6 @@ func (b *Buffer[T]) IsFull() bool {
 
 func (b *Buffer[T]) IsEmpty() bool {
 	return b.Len() == 0
-}
-
-func (b *Buffer[T]) AppendSlice(s []T) {
-	if !b.IsFull() {
-		b.data = append(b.data, s...)
-	} else {
-
-		panic("AppendSlice(s []T): buffer is full")
-	}
-}
-
-func (b *Buffer[T]) Append(s T) {
-	if !b.IsFull() {
-		b.data = append(b.data, s)
-	} else {
-		panic("Append(s T): buffer is full")
-	}
 }
 
 func NewByteBuffer(cap int) Buffer[byte] {
--- buffer_test.go
+++ buffer_test.go
@@ -1,16 +1,31 @@
 package buffer
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBuffer(t *testing.T) {
+	assert := assert.New(t)
+
 	cap := 5
 	b := NewByteBuffer(cap)
-	s := []byte{1, 2}
+	s := []byte{50, 51, 52, 53, 54}
+
 	b.AppendSlice(s)
 
-	if len(b.data) != len(s) {
-		t.Error("buffer does not equal slice")
-	}
+	assert.Equal(len(s), len(b.data))
+	assert.True(b.IsFull())
+	assert.False(b.IsEmpty())
 
-	t.Logf("buffer: %v\n", b.data)
+	v := b.PopRight()
+
+	assert.Equal(s[4], v)
+	assert.Len(b.data, cap-1)
+
+	v2 := b.PopLeft()
+
+	assert.Equal(s[0], v2)
+	assert.Len(b.data, cap-2)
 }
